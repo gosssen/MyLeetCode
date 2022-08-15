d65244034adcbb2e3cebde43f83dbc4b0f19cd7e
gosssen <inoplay.tw@gmail.com>
2022-08-15 17:43:48 +0800

    create TwoSum.java

--- src/main/java/com/gosssen/myleetcode/easy/TwoSum.java
+++ src/main/java/com/gosssen/myleetcode/easy/TwoSum.java
@@ -7,7 +7,7 @@ public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        int neededValue = 0;
+        int neededValue;
         for (int i = 0; i < nums.length; i++) {
             neededValue = target - nums[i];
             if (map.containsKey(neededValue)) {
