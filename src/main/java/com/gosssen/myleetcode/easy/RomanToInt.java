e9f515f965f3fb4edd4ad81c2750cdd36d6e5db7
gosssen <inoplay.tw@gmail.com>
2022-08-16 02:13:34 +0800

    create LongestCommonPrefix.java create RomanToInt.java

---
+++ src/main/java/com/gosssen/myleetcode/easy/RomanToInt.java
@@ -0,0 +1,126 @@
+package com.gosssen.myleetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInt {
+
+    /*  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+        Symbol       Value
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000
+
+        For example, 2 is written as II in Roman numeral, just two ones added together.
+        12 is written as XII, which is simply X + II.
+        The number 27 is written as XXVII, which is XX + V + II.
+        Roman numerals are usually written largest to smallest from left to right.
+        However, the numeral for four is not IIII.
+        Instead, the number four is written as IV.
+        Because the one is before the five we subtract it making four.
+        The same principle applies to the number nine, which is written as IX.
+
+        There are six instances where subtraction is used:
+        I can be placed before V (5) and X (10) to make 4 and 9.
+        X can be placed before L (50) and C (100) to make 40 and 90.
+        C can be placed before D (500) and M (1000) to make 400 and 900.
+        Given a roman numeral, convert it to an integer.
+    */
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+
+    // best
+    public int romanToInt2(String s) {
+        int len = s.length();
+        int val = 0;
+        int num = 0;
+        int prev;
+        len--;
+        switch (s.charAt(len)) {
+            case 'I':
+                val = 1;
+                break;
+            case 'V':
+                val = 5;
+                break;
+            case 'X':
+                val = 10;
+                break;
+            case 'L':
+                val = 50;
+                break;
+            case 'C':
+                val = 100;
+                break;
+            case 'D':
+                val = 500;
+                break;
+            case 'M':
+                val = 1000;
+                break;
+        }
+        prev = val;
+        num += val;
+
+        while (len-- != 0) {
+            switch (s.charAt(len)) {
+                case 'I':
+                    val = 1;
+                    break;
+                case 'V':
+                    val = 5;
+                    break;
+                case 'X':
+                    val = 10;
+                    break;
+                case 'L':
+                    val = 50;
+                    break;
+                case 'C':
+                    val = 100;
+                    break;
+                case 'D':
+                    val = 500;
+                    break;
+                case 'M':
+                    val = 1000;
+                    break;
+            }
+            if (val >= prev) {
+                num += val;
+                prev = val;
+            } else {
+                num -= val;
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RomanToInt().romanToInt("XXVII"));
+    }
+
+}
